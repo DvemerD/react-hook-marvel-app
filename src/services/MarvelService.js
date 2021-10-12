eb926a8048128063b1b05cde8cd8b6a723428f04
Danil Rybkin <teddychanel03@gmail.com>
2021-10-12 21:44:49 +0300

    get resourse from the server

---
+++ src/services/MarvelService.js
@@ -0,0 +1,25 @@
+
+
+export default class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=088cfd0035a16408f56dc70fa3829ade';
+
+    getResourse = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
