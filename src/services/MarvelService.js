5e3fd53bfda16632f666c08a976d0768a5663cb4
Danil Rybkin <teddychanel03@gmail.com>
2021-10-15 16:50:35 +0300

    add spinner and error message

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -14,12 +14,25 @@ export default class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        console.log(id);
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (res) => {
+        return {
+            name: res.name,
+            description: res.description,
+            thumbnail: res.thumbnail.path + '.' + res.thumbnail.extension,
+            homepage: res.urls[0].url,
+            wiki: res.urls[1].url   
+        }
     }
 }
 
